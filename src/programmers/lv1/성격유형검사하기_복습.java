b810c570f9111ae635938049ea96988d13e9b7bd
yul <yuyu409300@naver.com>
2023-07-04 22:03:39 +0900

    commit

---
+++ src/programmers/lv1/성격유형검사하기_복습.java
@@ -0,0 +1,39 @@
+package programmers.lv1;
+import java.util.*;
+class 성격유형검사하기_복습 {
+    public String solution(String[] survey, int[] choices) {
+
+        int R_score = 0; int T_score = 0; int C_score = 0; int F_score = 0;
+        int J_score = 0; int M_score = 0; int A_score = 0; int N_score = 0;
+
+        for(int i=0; i<survey.length; i++){
+            String type = survey[i].substring(0,1);
+            if(choices[i] > 4) type = survey[i].substring(1,2);
+
+            int score = Math.abs(choices[i]-4);
+
+            switch(type){
+                case "R" : R_score+=score; break;
+                case "T" : T_score+=score; break;
+                case "C" : C_score+=score; break;
+                case "F" : F_score+=score; break;
+                case "J" : J_score+=score; break;
+                case "M" : M_score+=score; break;
+                case "A" : A_score+=score; break;
+                case "N" : N_score+=score; break;
+            }
+        }
+
+        String type1 = "R";
+        String type2 = "C";
+        String type3 = "J";
+        String type4 = "A";
+
+        if(R_score < T_score) type1 = "T";
+        if(C_score < F_score) type2 = "F";
+        if(J_score < M_score) type3 = "M";
+        if(A_score < N_score) type4 = "N";
+
+        return type1+type2+type3+type4;
+    }
+}
