85ffb457915920852dfb375a129a7a8b52f90535
yul <yuyu409300@naver.com>
2023-07-09 23:15:43 +0900

    commit

---
+++ src/programmers/lv1/햄버거만들기_복습.java
@@ -0,0 +1,22 @@
+package programmers.lv1;
+import java.util.*;
+
+class 햄버거만들기_복습 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int x : ingredient){
+            stack.push(x);
+            if(stack.size() >=4){
+                if(stack.get(stack.size()-4)==1 && stack.get(stack.size()-3)== 2 && stack.get(stack.size()-2)== 3 && stack.get(stack.size()-1)== 1){
+                    answer++;
+                    stack.pop(); stack.pop(); stack.pop(); stack.pop();
+                }
+            }
+        }
+
+        return answer;
+    } // end of solution
+}
+
