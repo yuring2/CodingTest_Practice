cd9caec5e8237680748057f3183aca6e302fe191
yul <yuyu409300@naver.com>
2023-07-10 22:12:25 +0900

    commit

---
+++ src/programmers/lv1/명예의전당_복습.java
@@ -0,0 +1,31 @@
+package programmers.lv1;
+import java.util.*;
+
+class 명예의전당_복습 {
+    public int[] solution(int k, int[] score) {
+
+        int[] answer = new int[score.length];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<score.length; i++){
+            if(list.size() < k){
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i] = list.get(0);
+                continue;
+            }
+
+            if(list.size() == k){
+                int min = list.get(0);
+                if(score[i] > min){
+                    list.remove(0);
+                    list.add(score[i]);
+                    Collections.sort(list);
+                }
+                answer[i] = list.get(0);
+            }
+        }
+        return answer;
+    }
+}
+
