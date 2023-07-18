275e8ae38a3824622c6d8f5a7aa133a3e26af21e
yul <yuyu409300@naver.com>
2023-07-18 23:12:25 +0900

    commit

---
+++ src/programmers/lv1/옹알이2_복습.java
@@ -0,0 +1,23 @@
+package programmers.lv1;
+import java.util.*;
+
+class 옹알이2_복습 {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String str : babbling){
+
+            if(str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama")) continue;
+
+            str = str.replace("aya"," ");
+            str = str.replace("ye"," ");
+            str = str.replace("woo"," ");
+            str = str.replace("ma"," ");
+            str = str.replace(" ","");
+
+            if(str.length()==0) answer++;
+        }
+
+        return answer;
+    }
+}
+
